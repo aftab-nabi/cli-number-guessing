3abed61a06751fa54fc8306c51b32eaa84326c64
aftab-nabi <128211827+aftab-nabi@users.noreply.github.com>
2024-06-18 18:09:08 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+// in package.json
+//   "publishConfig": {
+    // "access": "public"
+
+//   "bin": "index.js",
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*6+1);
+const answers = await inquirer.prompt([
+    {
+        name: "user_GuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6: ",
+    },
+]);
+
+if (answers.user_GuessedNumber === randomNumber) {
+    console.log("Your guess is correct🎈🎇🎆");
+}else{
+    console.log("Please try again😡");
+}
